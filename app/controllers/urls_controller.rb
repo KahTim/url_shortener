ccb4d9054372c7856bdcb4d646d6b94d39a1b773
KahTim <kahtim.tang@gmail.com>
2017-11-23 14:29:28 +0800

    shorten url done

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,7 +4,28 @@ class UrlsController < ApplicationController
 	end
 
 	def show
-		byebug
+		# byebug
 	  @url = Url.find(params[:id])
 	end
+
+	def new
+	  @url = Url.new
+	end
+
+	def create
+	  @url = Url.new(url_params)
+	  @url.shorten
+	  if @url.save
+	      redirect_to @url  
+	  else
+	  		puts @url.errors.full_messages
+	      render 'new'
+	  end   
+	end
+
+	private 
+	def url_params
+	    params.require(:url).permit(:long_url)
+	end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,13 @@
 class Url < ApplicationRecord
+
+	validates :long_url, presence: true
+	validates :short_url, :long_url, uniqueness: true
+	validates :long_url, format: {with: /(((ftp|http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/, message: "valid URLs only"}
+	
+	def shorten
+		self.short_url = [*('A'..'Z'),*('a'..'z'),*('0'..'9')].shuffle[0,5].join
+		# return x
+		# self.shorten = SecureRandom.base64
+	end
+
 end
